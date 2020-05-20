89df9b69075e8ff2448adb69be838d181bce1584
ASM1204 <sangminan1204@gmail.com>
2020-05-20 21:44:28 +0900

    삭제 테스트 7777

--- src/main/java/com/jojoldu/book/springboot/config/auth/dto/SessionUser.java
+++ src/main/java/com/jojoldu/book/springboot/config/auth/dto/SessionUser.java
@@ -1,6 +1,5 @@
 package com.jojoldu.book.springboot.config.auth.dto;
 
-import com.jojoldu.book.springboot.domain.user.Role;
 import com.jojoldu.book.springboot.domain.user.User;
 import lombok.Getter;
 
@@ -11,12 +10,10 @@ public class SessionUser implements Serializable {
     private String name;
     private String email;
     private String picture;
-    private Role role;
 
     public SessionUser(User user) {
         this.name = user.getName();
         this.email = user.getEmail();
         this.picture = user.getPicture();
-        this.role = user.getRole();
     }
 }
