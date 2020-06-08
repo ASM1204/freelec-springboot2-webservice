08fdf0ce2fc4d004c74bbac7c2058012aefa3a62
ASM1204 <sangminan1204@gmail.com>
2020-06-09 02:44:40 +0900

    전적 테스트 6666666

--- src/main/java/com/jojoldu/book/springboot/web/IndexController.java
+++ src/main/java/com/jojoldu/book/springboot/web/IndexController.java
@@ -3,6 +3,7 @@ package com.jojoldu.book.springboot.web;
 import com.jojoldu.book.springboot.config.auth.LoginUser;
 import com.jojoldu.book.springboot.config.auth.dto.SessionUser;
 import com.jojoldu.book.springboot.service.posts.PostsService;
+import com.jojoldu.book.springboot.service.posts.RankService;
 import com.jojoldu.book.springboot.web.dto.PostsResponseDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
@@ -16,6 +17,7 @@ import javax.servlet.http.HttpSession;
 public class IndexController {
 
     private final PostsService postsService;
+    private final RankService rankService;
     private final HttpSession httpSession;
 
 
@@ -30,7 +32,7 @@ public class IndexController {
 
     @GetMapping("/")
     public String Rank(Model model, @LoginUser SessionUser user) {
-        model.addAttribute("rank", postsService.findRank());
+        model.addAttribute("rank", rankService.findRank());
         return "index";
     }
 
