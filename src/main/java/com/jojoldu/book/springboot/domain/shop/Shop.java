11a0ada54a1562e14cc02852da5e9252fe24f63f
ASM1204 <sangminan1204@gmail.com>
2020-06-10 19:23:26 +0900

    상점테스트 10

--- src/main/java/com/jojoldu/book/springboot/domain/shop/Shop.java
+++ src/main/java/com/jojoldu/book/springboot/domain/shop/Shop.java
@@ -36,6 +36,15 @@ public class Shop extends BaseTimeEntity {
         this.item_price = item_price;
     }
 
+    public Shop List(Long id, String item_name, String item_image, String item_type, int item_price) {
+        this.id = id;
+        this.item_name = item_name;
+        this.item_image = item_image;
+        this.item_type = item_type;
+        this.item_price = item_price;
+        return this;
+    }
+
     public void buy(String item_name, int item_price) {
         this.item_name = item_name;
         this.item_price = item_price;
