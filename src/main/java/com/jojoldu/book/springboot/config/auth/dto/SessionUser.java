1a9a56873f8f2bb567ec132def347196fb6a13f5
ASM1204 <sangminan1204@gmail.com>
2020-06-17 16:20:34 +0900

    user test 15

--- src/main/java/com/jojoldu/book/springboot/config/auth/dto/SessionUser.java
+++ src/main/java/com/jojoldu/book/springboot/config/auth/dto/SessionUser.java
@@ -9,13 +9,13 @@ import java.io.Serializable;
 public class SessionUser implements Serializable {
     private static final long serialVersionUID = 1L;
 
-    private String user_id;
+    private Long user_id;
     private String name;
     private String email;
     private String picture;
 
     public SessionUser(User user) {
-        this.user_id = String.valueOf(user.getUser_id());
+        this.user_id = user.getUser_id();
         this.name = user.getName();
         this.email = user.getEmail();
         this.picture = user.getPicture();
