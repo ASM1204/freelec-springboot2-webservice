c10f0f4010f5dca26c0de5a262b6d443bec23a1e
ASM1204 <sangminan1204@gmail.com>
2020-06-17 17:35:07 +0900

    user test 17

--- src/main/java/com/jojoldu/book/springboot/service/posts/UserService.java
+++ src/main/java/com/jojoldu/book/springboot/service/posts/UserService.java
@@ -6,6 +6,7 @@ import com.jojoldu.book.springboot.domain.shop.ShopRepository;
 import com.jojoldu.book.springboot.domain.user.User;
 import com.jojoldu.book.springboot.domain.user.UserRepository;
 import com.jojoldu.book.springboot.domain.user.UserRepository2;
+import com.jojoldu.book.springboot.domain.user.UserRepository3;
 import com.jojoldu.book.springboot.web.dto.*;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -19,6 +20,7 @@ import java.util.stream.Collectors;
 @Service
 public class UserService {
     private final UserRepository2 userRepository2;
+    private final UserRepository3 userRepository3;
 
     @Transactional
     public Long buy(Long user_id, UserRequestDto requestDto) {
@@ -29,7 +31,7 @@ public class UserService {
 
 
     public UserResponseDto findById(Long user_id) {
-        User entity = userRepository2.findById(user_id).orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id = " + user_id));
+        User entity = userRepository3.findById(user_id).orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id = " + user_id));
         return new UserResponseDto(entity);
     }
 
